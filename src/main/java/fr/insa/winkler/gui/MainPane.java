4b9e47727bdccbb2794f93ed84a25674f99d592b
marie-project <winkler.marie@icloud.com>
2022-11-02 16:56:52 +0100

    qq modifs

--- src/main/java/fr/insa/winkler/gui/MainPane.java
+++ src/main/java/fr/insa/winkler/gui/MainPane.java
@@ -5,27 +5,12 @@
  */
 package fr.insa.winkler.gui;
 
-import java.io.File;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.MenuButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -39,18 +24,21 @@ public class MainPane extends BorderPane{
     private Button bConnexion;  
     private Button bNouvelUtilisateur; 
     private TextArea message; 
+    private ScrollPane mainContent;
 
     private Stage stage;
     
     
     public MainPane(Stage st){
         this.stage=st;
+        this.mainContent=new ScrollPane();
         this.mbUtilisateur=new MenuButton ("Utilisateur");
         this.message=new TextArea();
         this.message.setMinHeight(70);
         this.message.setMaxHeight(70);
         this.setTop(this.mbUtilisateur);
         this.setBottom(this.message);
+        this.setCenter(this.mainContent);
         
         MenuItem menuItemConnexion = new MenuItem("Connexion");
         MenuItem menuItemNouvelUtilisateur = new MenuItem("Créer un compte");
